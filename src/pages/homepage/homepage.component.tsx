aff305c7de00f7270490690c1dc4f70c0c836d90
Brixton <lkw06182000@hotmail.com>
2022-08-20 19:05:54 +0800

    Complete hero section and card section

---
+++ src/pages/homepage/homepage.component.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroSection from "../../components/hero-section/hero-section.component";
+import './homepage.styles.scss';
+const Homepage:React.FC = () => {
+    return(
+        <div>
+            <HeroSection/>
+        </div>
+    )
+}
+
+export default Homepage;
